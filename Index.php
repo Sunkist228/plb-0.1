cafa23376ed42fc2411a1f1187fbb38151128cab
Sunkist <145262380+Sunkist228@users.noreply.github.com>
2023-12-06 21:22:00 +0300

    web-news created

--- Index.php
+++ Index.php
@@ -1,29 +1,31 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" href="style.css">
-	<title>Сайт с базой данных</title>
+	<title>Сайт с новостями</title>
 </head>
-
 <body>
 	<div class="container">
-		<h1>Пользователи</h1>
+		<h1>Новости</h1>
 		<ul>
 			<?php
 			include('db_connection.php');
 
-			$sql = "SELECT * FROM users";
+			$sql = "SELECT * FROM news ORDER BY publish_date DESC";
 			$result = $conn->query($sql);
 
 			if ($result->num_rows > 0) {
 				while ($row = $result->fetch_assoc()) {
-					echo "<li>" . $row["name"] . " - " . $row["email"] . "</li>";
+					echo "<li>";
+					echo "<h2>" . $row["title"] . "</h2>";
+					echo "<p>" . $row["content"] . "</p>";
+					echo "<p class='date'>Дата публикации: " . $row["publish_date"] . "</p>";
+					echo "</li>";
 				}
 			} else {
-				echo "<li>Нет пользователей</li>";
+				echo "<li>Нет новостей</li>";
 			}
 
 			$conn->close();
@@ -31,5 +33,4 @@
 		</ul>
 	</div>
 </body>
-
 </html>
